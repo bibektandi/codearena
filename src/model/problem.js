44aa918f2f664e6ba7cb9afa84e50ecf9fc03adf
bibektandi <tandibibek20@gmail.com>
2026-06-12 18:05:34 +0530

    probelm schema

---
+++ src/model/problem.js
@@ -0,0 +1,71 @@
+const mongoose=require('mongoose')
+const {Schema}=mongoose;
+
+const problemSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+    type:String,
+    required:true
+    },
+    difficulty:{
+     type:String,
+     required:true
+    },
+    tags:{
+    type:String,
+    required:true,
+    },
+    Visibletestcases:[
+        {
+            input:{
+              type:String,
+              required:true,
+            },
+            output:{
+               type:String,
+               required:true
+            },
+            explanation:{
+                type:String,
+                required:true
+            }
+
+        }
+    ],
+    hiddentestcases:[
+        {
+            input:{
+              type:String,
+              required:true,
+            },
+            output:{
+               type:String,
+               required:true
+            }
+
+        }
+    ],
+    startcode:[{
+        language:{
+            type:String,
+            required:true
+        },
+        initialcode:{
+            type:String,
+            required:true
+        }
+
+    }],
+    problemcreator:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    }
+
+})
+
+const problem=mongoose.model('problem',problemSchema);
+module.exports=problem;
